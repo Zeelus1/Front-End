cbf5987bbc2d528d01010ddbcf85f21ac477b028
AndreyJustino <andreyjustino2020@gmail.com>
2025-05-10 17:14:07 -0300

    Add Cadastro, LandingPage, and Login components with corresponding CSS modules

---
+++ src/pages/cadastro/Cadastro.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Style from "./Cadastro.module.css"
+
+function Cadastro() {
+  return (
+    <div>Cadastro</div>
+  )
+}
+
+export default Cadastro
---
+++ src/pages/landingPage/LandingPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Style from "./LandingPage.module.css"
+
+function LandingPage() {
+  return (
+    <div>LandingPage</div>
+  )
+}
+
+export default LandingPage
