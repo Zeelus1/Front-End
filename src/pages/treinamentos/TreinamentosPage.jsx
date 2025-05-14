f57d73dacd57fab920b32ff8ce75805f9a65da9f
William Ferreira <williamferreira2004.contato@gmail.com>
2025-05-14 18:56:20 -0300

    adicionei uma imagem

--- src/pages/treinamentos/TreinamentosPage.jsx
+++ src/pages/treinamentos/TreinamentosPage.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import Style from './TreinamentosPage.module.css'
+import ImgCoracaoComUmaMao from "../../img/coracaoComUmaMao.png"
+
 function TreinamentosPage() {
   return (
-    <div>oi</div>
+    <section>
+      <div>
+        <img src={ImgCoracaoComUmaMao} alt="Imagem de uma mão segurando um coração" />
+        <h1></h1>
+      </div>
+    </section>
   )
 }
 
