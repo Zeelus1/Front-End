d3b58bc891d74465d1475e0e156401b916c8da5a
AndreyJustino <andreyjustino2020@gmail.com>
2025-05-14 21:13:25 -0300

    fix: corrigindo caminhos de arquivos de imagem  e atualizando .gitignore para vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from "react-router";
-import router from './Routes/Routes.jsx';
+import router from './routes/Routes.jsx';
 
 createRoot(document.getElementById('root')).render(
   <RouterProvider router={router}/>
