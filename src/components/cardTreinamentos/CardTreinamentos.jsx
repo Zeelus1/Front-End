4e8cfab182ffe239798394fc98ea51550ead075f
AndreyJustino <andreyjustino2020@gmail.com>
2025-06-25 20:28:42 -0300

    update: atualizando opções de genero em componente ModalAnamnese para inclusão

--- src/components/cardTreinamentos/CardTreinamentos.jsx
+++ src/components/cardTreinamentos/CardTreinamentos.jsx
@@ -3,7 +3,7 @@ import styles from './CardTreinamentos.module.css';
 import iconeTreina from '../../img/iconeTreina.png';
 import engasgo from '../../img/engasgo.png';
 import sombra from '../../img/sombra.png';
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router';
 
 export default function CardTreinamento() {
   return (
